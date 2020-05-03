5ec80f96b838ef70ec2a4df3fb401a81ea85bfdb
Anusit <hyongshu@gmail.com>
2020-05-03 18:21:38 +0700

    Updates

---
+++ precache-manifest.055987d41d0c3657ffe8dfaff7fef8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7624df3ce48dc3c59c29162b8e049f",
+    "url": "/yesnews/index.html"
+  },
+  {
+    "revision": "f723db7da28a47cc459b",
+    "url": "/yesnews/static/css/main.23e67b88.chunk.css"
+  },
+  {
+    "revision": "9da9168e606b2a8f2266",
+    "url": "/yesnews/static/js/2.8cbe562c.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/yesnews/static/js/2.8cbe562c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f723db7da28a47cc459b",
+    "url": "/yesnews/static/js/main.3086a162.chunk.js"
+  },
+  {
+    "revision": "49bc8adf141d0299cc1e",
+    "url": "/yesnews/static/js/runtime-main.d9a10708.js"
+  }
+]);
